a811ab04fae944186df0e6f6c97486c127891754
Henrique Vilela <39452510+vilelalabs@users.noreply.github.com>
2021-09-02 16:24:20 -0300

    Add files via upload

---
+++ BytesPadroes.h
@@ -0,0 +1,36 @@
+
+/*- NESTE ARQUIVO ESTAO DEFINIDOS OS VALORES PARA OS BYTES ---------------------------
+----------------------------------------------- NAO ESCANEADOS DO ARQUIVO .UP0 FINAL-*/
+
+#ifndef _BYTES_PADROES_H_
+#define _BYTES_PADROES_H_
+
+#define BYTE_PADRAO_04 90      // Provavelmente MDrums Volume
+#define BYTE_PADRAO_05 90       // Arranger Drums Volume
+#define BYTE_PADRAO_06 90       // Arranger Bass Volume
+#define BYTE_PADRAO_07_A_12 90  // Arranger Accomp. Volume
+#define BYTE_PADRAO_13 0        // Função Não Verificada
+#define BYTE_PADRAO_17 0        // Provavelmente ativa outros efeitos como Rev e Chr
+#define BYTE_PADRAO_18 0        // Provavelmente ativa outros efeitos como Rev e Chr
+#define BYTE_PADRAO_19 0        // Provavelmente ativa outros efeitos como Rev e Chr
+#define BYTE_PADRAO_21_A_37 0   // Função Não Verificada
+#define BYTE_PADRAO_39 0        // Função Não Verificada
+#define BYTE_PADRAO_41 0        // Função Não Verificada
+#define BYTE_PADRAO_43 0        // Função Não Verificada
+#define BYTE_PADRAO_45 0        // Função Não Verificada
+#define BYTE_PADRAO_47 15       // Style A
+#define BYTE_PADRAO_48 1        // Função Não Verificada
+#define BYTE_PADRAO_49 1        // Função Não Verificada
+#define BYTE_PADRAO_50 0        // Style B
+#define BYTE_PADRAO_52 4        // Função Não Verificada
+#define BYTE_PADRAO_53 4        // Função Não Verificada
+#define BYTE_PADRAO_54 0        // Função Não Verificada
+#define BYTE_PADRAO_55 3        // Função Não Verificada
+#define BYTE_PADRAO_57 -1       // LOWER (-1=ON, -5=OFF) Lower sempre ON (mas não necessáriamente ativado)
+#define BYTE_PADRAO_59 5        // Style Orchestration
+#define BYTE_PADRAO_60 0        // Style Morph
+#define BYTE_PADRAO_62_A_73 64  // Provavelmente PANControl de todas partes (não testado)
+#define BYTE_PADRAO_74 24       // Arranger Split
+#define BYTE_PADRAO_76 66       // Função Não Verificada
+
+#endif
